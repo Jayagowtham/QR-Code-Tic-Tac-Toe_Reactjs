9ee566c91e91502df39f2bb65c3916cdcb118807
Jayagowtham <jayagowtham005@gmail.com>
2024-03-07 19:36:11 +0530

    mini projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import QRCodeGenerator from './components/QR Code Generator';
+import TicTacToe from './components/Tic_Tac_Toe';
+import ProfileDetailsSection from './components/profilepure/profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*------- QR CodeGenerator ------*/ }
+      {/* <QRCodeGenerator/> */}
+
+      {/*----------Tic Tac Toe ----------*/}
+      <TicTacToe/>
+      
     </div>
   );
 }
